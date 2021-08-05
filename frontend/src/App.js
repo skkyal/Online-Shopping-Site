8732567dc5ce4087315ad07e2816f60890218aca
Shlok Kyal <shlok.kyal@gmail.com>
2021-08-05 21:30:46 +0530

    Added basic structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App"></div>;
-}
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ShopCart</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ShopCart</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i> SignIn
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
