6eeb9fd59af25222cd7f3b1073975eeb289f5b26
Shlok Kyal <shlok.kyal@gmail.com>
2021-08-12 17:58:35 +0530

    Completed Authorization and Authentication Backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { authUser } from "../controllers/userControllers.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userControllers.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
